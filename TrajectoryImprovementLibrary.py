5dc5ce80abd8d9002ecdb11828adc392e3179f4d
Petar Tushev <tushevio@hotmail.com>
2022-05-23 14:57:14 +0200

    Genetic algorithm added.

--- TrajectoryImprovementLibrary.py
+++ TrajectoryImprovementLibrary.py
@@ -10,6 +10,7 @@ from sys import path_importer_cache
 import numpy as np
 import random
 import math
+from itertools import tee
 
 
 
@@ -22,19 +23,24 @@ def TrajectoryImprovement( originalTraj, variablePoints, optAlgorithm ):
         newTraj = Algorithm_Random( originalTraj, variablePoints, auxParameters )
 
     if optAlgorithm == 'hill_climb':
-        auxParameters = { 'numIters': 1000 }
+        auxParameters = { 'numIters': 10 }
         newTraj = Algorithm_Hill_Climb( originalTraj, variablePoints, auxParameters )
 
     if optAlgorithm == 'simulated_annealing':
         auxParameters = { 'numIters': 1 }
-        newTraj = Algorithm_Simulated_Annealing( originalTraj, variablePoints, auxParameters )
+        newTraj = Algorithm_Simulated_Annealing( originalTraj, variablePoints )
 
     if optAlgorithm == 'tabu':
         auxParameters = { 'numIters': 1000 }
         newTraj = Algorithm_Tabu_Search( originalTraj, variablePoints, auxParameters )
 
     if optAlgorithm == 'local_beam':
-        auxParameters = { 'numIters': 1000 }
+        auxParameters = { 'numIters': 1 }
+        newTraj = Algorithm_Local_Beam( originalTraj, variablePoints, auxParameters )
+
+
+    if optAlgorithm == 'genetic':
+        auxParameters = { 'numIters': 1 }
         newTraj = Algorithm_Local_Beam( originalTraj, variablePoints, auxParameters )
     
     
@@ -55,8 +61,8 @@ def Algorithm_Random( trajectory, variablePoints, auxParameters ):
         tTraj = np.copy( trajectory )
         for k in range( np.size( trajectory, 0 ) ):
             if variablePoints[ k ]:
-                tTraj[ k, 1 ] += np.random.normal( 0.0, .001 )
-                tTraj[ k, 2 ] += np.random.normal( 0.0, .001 )
+                tTraj[ k, 1 ] += np.random.normal( 0.0, .01 )
+                tTraj[ k, 2 ] += np.random.normal( 0.0, .01 )
                 
         newError = aux_CalculateTrajectoryError( tTraj, variablePoints )
         
@@ -89,7 +95,7 @@ def Algorithm_Hill_Climb( trajectory, variablePoints, auxParameters ):
             
             if variablePoints[k]:
 
-                neighbours = getNeighbours( trajectory[k], auxParameters )
+                neighbours = getNeighbours( trajectory[k] )
 
 
                 lowest_error = aux_CalculateTrajectoryError( t1Traj, variablePoints )
@@ -123,30 +129,11 @@ def Algorithm_Hill_Climb( trajectory, variablePoints, auxParameters ):
 
 
 
-def getNeighbours( point, auxParameters ):
-    neighbours = []
-
-    for _ in range(auxParameters[ 'numIters' ]):
-
-        neighbour = np.copy( point )
-
-        neighbour[1] += np.random.normal( 0, .001 )
-        neighbour[2] += np.random.normal( 0, .001 )
-
-        neighbours.append(neighbour)
-
-    return neighbours
-
-
-
-
-
-
-def Algorithm_Simulated_Annealing( originalTraj, variablePoints, auxParameters ):
+def Algorithm_Simulated_Annealing( originalTraj, variablePoints ):
 
     initial_temp = 90
     final_temp = .1
-    alpha = .01
+    alpha = .1
 
     current_temp = initial_temp
 
@@ -154,6 +141,7 @@ def Algorithm_Simulated_Annealing( originalTraj, variablePoints, auxParameters )
     
 
     while current_temp > final_temp:
+        print(f'Current temp: {current_temp}')
 
 
         for k in range( originalTraj.shape[0] ):
@@ -162,7 +150,7 @@ def Algorithm_Simulated_Annealing( originalTraj, variablePoints, auxParameters )
             
             if variablePoints[k]:
 
-                neighbour = random.choice(getNeighbours(solution[k], auxParameters))
+                neighbour = random.choice(getNeighbours( solution[k] ))
 
                 solution[k + 1] = neighbour
 
@@ -195,20 +183,24 @@ def Algorithm_Tabu_Search( originalTraj, variablePoints, auxParameters ):
     # aspirationCriteria = 2
     tabuTenure = 20
     tabuList = dict()
+    
 
     for k in range( len( originalTraj ) ):
+        
+        bestNeighbour = None
 
         currTraj = np.copy( bestTraj )
 
         if variablePoints[k]:
 
-            neighbours = getNeighbours( currTraj[k], auxParameters )
+            neighbours = getNeighbours( currTraj[k] )
 
             # neighbours = filter(lambda x: aspirationCriteria(x))
 
-            bestNeighbour = None
+            
 
             for neighbour in neighbours:
+                # print(f'Neighbour: {neighbour}')  
 
                 currTraj[k + 1] = neighbour
 
@@ -216,20 +208,27 @@ def Algorithm_Tabu_Search( originalTraj, variablePoints, auxParameters ):
 
                 cost_diff = Error - error
 
-                if cost_diff <= 0:
-
+                if cost_diff >= 0:
+                    # print('New path found.')
                     bestTraj = np.copy( currTraj )
 
                     bestNeighbour = neighbour
 
-        for key in tabuList.keys():
+
+        for key in tabuList.copy().keys():
             
             tabuList[key] -= 1
             
             if tabuList[key] == 0:
                 del tabuList[key]
+                # to_be_removed.append(key)
 
-        tabuList[bestNeighbour] = tabuTenure
+        if bestNeighbour is not None:
+            # print(bestNeighbour)
+            tabuList[bestNeighbour.tobytes()] = tabuTenure
+
+        # for key in to_be_removed:
+        #     del tabuList[key]
 
     return bestTraj
 
@@ -238,26 +237,144 @@ def Algorithm_Tabu_Search( originalTraj, variablePoints, auxParameters ):
 
 
 
-def aspirationCriteria(point):
-    raise NotImplementedError()
+# def aspirationCriteria(point):
+#     raise NotImplementedError()
 
 
 
 def Algorithm_Local_Beam( originalTraj, variablePoints, auxParameters ):
-    raise NotImplementedError
+    print()
 
 
 def Genetic_Algorithm( originalTraj, variablePoints, auxParameters ):
-    # prvo ke gi najdam site True variable variablePoints
-    # za sekoj variablePoint, vklucuvajki go i veke postoeckio, 
-    # ke im napravam selekcija so nekojasi zasega nespecificirana fitness funkcija
-    # ke napravam vkrstuvanje i na odreden procent ke napravam mutacija
-    # i taka tava ke go praam 1000 pati
-    # posle tava, za sekoj variable point ke ga izberam najdobrata tocka
-    # ke ga vratam traektorijata
+    # 1. Inicijalna populacija: treba za variablePoints deka so se True, da izgeneriram odreden broj na edinki
+    # 2. So fitnes funkcijata, u taj slucaj aux_CalculateErrorSinglePoint, za sekoja ednika da presmetam kolku e dobra
+    # 3. Ko ke presmetam fitness, treba da go zacuvam toa vo dict i da gi sortiram spored vrednosta. Prvata polovina ke bide odbrana za ponatamu.
+    # 4. treba pomegju edinkite nekako da napravam crossover t.e. nekako da gi vkrstam za da dobijam duplo
+    # 5. Treba da napravam so odreden procent od novite edinki da se imat mutirano
 
-    raise NotImplementedError()
 
+    bestTraj = np.copy( originalTraj )
+
+    best_units = dict()
+
+    for iter in range(auxParameters[ 'numIters' ]):
+
+        # print('STEP 1')
+
+        d = dict()
+
+        for k in range(len( originalTraj )):                            # STEP 1
+
+            if variablePoints[k]:
+
+                d[k] = getNeighbours(originalTraj[k])
+
+
+        index_to_errors = dict()
+
+
+        # print('STEP 2')
+
+        for key in d.keys():                                            # STEP 2
+
+            tTraj = np.copy( bestTraj )
+
+            individual_errors = dict()
+
+            for individual in d[key]:
+
+                tTraj[key] = individual
+
+                error = aux_CalculateTrajectoryError(tTraj, variablePoints)
+
+                individual_errors[individual.tobytes()] = error
+
+                if key in best_units.keys():
+
+                    tTraj[key] = best_units[key]
+
+                    best_error = aux_CalculateTrajectoryError(tTraj, variablePoints)
+
+                    if best_error > error:
+
+                        best_units[key] = individual
+
+                else:
+
+                    best_units[key] = individual
+
+            index_to_errors[key] = individual_errors.copy()
+
+        index_to_errors_sorted = dict()
+        selected_individuals = dict()
+
+
+        # print('STEP 3')
+
+        for key in index_to_errors.keys():                              # STEP 3
+
+            index_to_errors_sorted[key] = sorted(index_to_errors[key].items(), key=lambda x: x[1], reverse=False)
+
+            selected_individuals[key] = dict(list(index_to_errors_sorted[key])[:int(len(index_to_errors_sorted[key])/2)])
+
+
+        # print('STEP 4')
+
+        for key in selected_individuals.keys():                         # STEP 4
+            
+            #treba da dojdam do edinkite, da gi vratam vo float64 ndarray format od bytes, error ne mi treba veke
+            # treba da gi zemam dva po dva elementite, i da gi zamenam mestata na lon koordinatite
+
+            keys = selected_individuals[key].keys()
+
+            keys = np.array([np.frombuffer(x) for x in keys])
+
+            for k in range(0, len(keys), 2):
+                            
+                keys[k][2], keys[k + 1][2] = keys[k + 1][2], keys[k][2]
+
+                # print('STEP 5')
+
+                if random.random() < .2:                                # STEP 5
+
+                    keys[k][1] += np.random.normal(0, .001)
+                    keys[k][2] += np.random.normal(0, .001)
+
+    for key, value in best_units.items():
+
+        bestTraj[key] = value
+
+    return bestTraj
+      
+
+               
+
+
+def getNeighbours( point ):
+    neighbours = []
+
+    for _ in range( 500 ):
+
+        neighbour = np.copy( point )
+
+        neighbour[1] += np.random.normal( 0, .01 )
+        neighbour[2] += np.random.normal( 0, .01 )
+
+        neighbours.append(neighbour)
+
+    return neighbours
+
+
+
+
+def pairwise(iterable):
+
+    a, b = tee(iterable)
+    next(b, None)
+
+    return zip(a, b)
+                
 
 
 
