3b3eef646fd1dcaaf21a5ad3124043ef57e5c2c4
Petar Tushev <tushevio@hotmail.com>
2023-02-27 09:00:36 +0100

    Changes to views.

--- web_app/map/views.py
+++ web_app/map/views.py
@@ -9,23 +9,17 @@ import numpy as np
 import pandas as pd
 import folium
 
+# from paths import *
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+print(BASE_DIR)
 
 scripts_path = str(BASE_DIR.parent) + '/scripts/'
 
 sys.path.insert(0, scripts_path)
 
 import TrajectoryImprovementLibrary as TIL
-
-# print(os.getcwd())
-
-
-# print(TIL.__path__)
-
-
-
-
-# Create your views here.
 
 
 def map(request):
@@ -34,16 +28,27 @@ def map(request):
     lat, lon = 41.987734, 21.428074
     zoom_start = 6
 
-    is_optimize_request = request.headers.get('optimize')
+    is_optimize_request, is_ajax, new_traj = None, None, None
 
-    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+    try:
+        is_optimize_request = request.headers.get('optimize')
+    except:
+        pass
+    
+    try:
+        is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+    except:
+        pass
+
+    try:
+        new_traj = request.headers.get('new-trajectory') == 'true'
+    except:
+        pass
 
     if is_optimize_request:
         
         if is_ajax:
             if request.method == 'POST':
-
-                print('OPTIMIZE CALLED')
 
                 data = json.load(request)
 
@@ -63,7 +68,7 @@ def map(request):
 
                 # --------------- HARDCODED VALUES FOR TRAJECTORY 1 ---------------
 
-                correctedTraj1 = TIL.TrajectoryImprovement( traj, variablePoints, data['algorithm'] )
+                correctedTraj1, corrected_error = TIL.TrajectoryImprovement( traj, variablePoints, data['algorithm'] )
 
                 tmp = traj[:, 1:3]
                 cTraj1 = correctedTraj1[:, 1:3]
@@ -83,38 +88,62 @@ def map(request):
                 aline2 = folium.PolyLine(cTraj1, color='red')
                 m.add_child(aline2)
 
-                print('RETURNED')
-                return JsonResponse({'new_map': m._repr_html_()}, safe=True)
+                return JsonResponse({'new_map': m._repr_html_(), 'corrected_error': corrected_error}, safe=True)
     else:
 
         if is_ajax:
-            if request.method == 'POST':
+
+            if new_traj:
+
+                all_trajectories = []
+                traj_path = str(BASE_DIR.parent) + '/SampleTraj/'
+                regex = re.compile('^Trj_.*.npy$')
+
+                for _, _, files in os.walk(traj_path):
+                    for file in files:
+                        if regex.match(file):
+                            all_trajectories.append(file)
+
+                
 
                 data = json.load(request)
 
-                trajectory_path = str(BASE_DIR.parent) + f'/SampleTraj/Trj_{int(data["num"])}.npy'
+                with open(str(BASE_DIR.parent) + f'/SampleTraj/Trj_{len(all_trajectories)}.npy', 'w') as f:
+                    f.write(data)
+                    f.close()
+                    
 
-                traj = np.load(trajectory_path)
-
-                traj = pd.DataFrame(traj)
-                traj.columns = ['timestamp', 'latitude', 'longitude', 'altitude']
-                traj = traj.drop_duplicates(subset=['latitude', 'longitude'], keep='first').to_numpy()
-
-                tmp = traj[:, 1:3]
                 
-                lat = np.mean([row[0] for row in tmp])
-                lon = np.mean([row[1] for row in tmp])
+            else:
+                if request.method == 'POST':
 
-                m = folium.Map(
-                    location=[lat, lon],
-                    tiles="cartodbpositron",
-                    zoom_start=zoom_start
-                )
+                    data = json.load(request)
 
-                aline1 = folium.PolyLine(tmp, color='blue')
-                m.add_child(aline1)
+                    trajectory_path = str(BASE_DIR.parent) + f'/SampleTraj/Trj_{int(data["num"])}.npy'
 
-                return JsonResponse({'new_map': m._repr_html_()}, safe=True)
+                    traj = np.load(trajectory_path)
+
+                    traj = pd.DataFrame(traj)
+                    traj.columns = ['timestamp', 'latitude', 'longitude', 'altitude']
+                    traj = traj.drop_duplicates(subset=['latitude', 'longitude'], keep='first').to_numpy()
+
+                    tmp = traj[:, 1:3]
+                    
+                    lat = np.mean([row[0] for row in tmp])
+                    lon = np.mean([row[1] for row in tmp])
+
+                    m = folium.Map(
+                        location=[lat, lon],
+                        tiles="cartodbpositron",
+                        zoom_start=zoom_start
+                    )
+
+                    aline1 = folium.PolyLine(tmp, color='blue')
+                    m.add_child(aline1)
+
+                    return JsonResponse({'new_map': m._repr_html_()}, safe=True)
+
+
 
     all_trajectories = []
     traj_path = str(BASE_DIR.parent) + '/SampleTraj/'
@@ -140,58 +169,63 @@ def map(request):
 
     return render(request, 'map/base.html', context)
 
-def optimize(request):
+# def optimize(request):
 
+#     lat, lon = 41.987734, 21.428074
+#     zoom_start = 6
 
+#     is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
 
-    lat, lon = 41.987734, 21.428074
-    zoom_start = 6
+#     if is_ajax:
+#         if request.method == 'POST':
 
-    is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+#             print('OPTIMIZE CALLED')
 
-    if is_ajax:
-        if request.method == 'POST':
+#             data = json.load(request)
 
-            print('OPTIMIZE CALLED')
+#             trajectory_path = str(BASE_DIR.parent) + f'/SampleTraj/Trj_{int(data["trajectories-select"])}.npy'
 
-            data = json.load(request)
+#             traj = np.load(trajectory_path)
 
-            trajectory_path = str(BASE_DIR.parent) + f'/SampleTraj/Trj_{int(data["trajectories-select"])}.npy'
-
-            traj = np.load(trajectory_path)
-
-            traj = pd.DataFrame(traj)
-            traj.columns = ['timestamp', 'latitude', 'longitude', 'altitude']
-            traj = traj.drop_duplicates(subset=['latitude', 'longitude'], keep='first').to_numpy()
+#             traj = pd.DataFrame(traj)
+#             traj.columns = ['timestamp', 'latitude', 'longitude', 'altitude']
+#             traj = traj.drop_duplicates(subset=['latitude', 'longitude'], keep='first').to_numpy()
             
-            # --------------- HARDCODED VALUES FOR TRAJECTORY 1 ---------------
+#             # --------------- HARDCODED VALUES FOR TRAJECTORY 1 ---------------
 
-            targetPoints = [260]
-            variablePoints = np.zeros( ( np.size( traj, 0 ) ), dtype = bool )
-            variablePoints[targetPoints] = True
+#             targetPoints = [260]
+#             variablePoints = np.zeros( ( np.size( traj, 0 ) ), dtype = bool )
+#             variablePoints[targetPoints] = True
 
-            # --------------- HARDCODED VALUES FOR TRAJECTORY 1 ---------------
+#             # --------------- HARDCODED VALUES FOR TRAJECTORY 1 ---------------
 
-            # correctedTraj1 = TIL.TrajectoryImprovement( traj, variablePoints, data['algorithms-select'] )
+#             # correctedTraj1 = TIL.TrajectoryImprovement( traj, variablePoints, data['algorithms-select'] )
 
-            tmp = traj[:, 1:3]
-            # cTraj1 = correctedTraj1[:, 1:3]
+#             tmp = traj[:, 1:3]
+#             # cTraj1 = correctedTraj1[:, 1:3]
 
-            lat = np.mean([row[0] for row in tmp])
-            lon = np.mean([row[1] for row in tmp])
+#             lat = np.mean([row[0] for row in tmp])
+#             lon = np.mean([row[1] for row in tmp])
 
-            m = folium.Map(
-                location=[lat, lon],
-                tiles="cartodbpositron",
-                zoom_start=zoom_start
-            )
+#             m = folium.Map(
+#                 location=[lat, lon],
+#                 tiles="cartodbpositron",
+#                 zoom_start=zoom_start
+#             )
 
-            aline1 = folium.PolyLine(tmp, color='blue')
-            m.add_child(aline1)
+#             aline1 = folium.PolyLine(tmp, color='blue')
+#             m.add_child(aline1)
 
-            # aline2 = folium.PolyLine(cTraj1, color='red')
-            # m.add_child(aline2)
+#             # aline2 = folium.PolyLine(cTraj1, color='red')
+#             # m.add_child(aline2)
 
-            print('RETURNED')
-            return JsonResponse({'new_map': m._repr_html_()}, safe=True)
+#             print('RETURNED')
+#             return JsonResponse({'new_map': m._repr_html_()}, safe=True)
 
+# def new_trajectory(request):
+
+#     new_traj = request.headers.get('new-trajectory') == 'true'
+
+#     if new_traj:
+
+#         print('test')
